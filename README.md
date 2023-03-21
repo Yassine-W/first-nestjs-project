debebaf0dfab6506fe0414b329e27e785eafc2d2
Yassine-W <ysellami@witekio.com>
2023-03-21 09:22:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Notes nest.js
+
+```bash
+nest new my-nest-project
+```
+
+This command will watch your files, automatically recompiling and reloading the server:
+
+```bash
+npm run start:dev
+```
+
+```bash
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+```
+
+## Creating controllers
+
+```bash
+nest g controller cats
+```
+
+## Default behaviour
+
+the response status code is always 200 by default, except for POST requests which are 201
+
+## Lint error
+
+```bash
+'prettier/prettier': ['error', {endOfLine: 'auto'}]
+```
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Redirect,
+} from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+
+@Controller('cats')
+// @Controller({ host: 'localhost:3000', path: 'cats' })
+export class CatsController {
+  //   @Post()
+  //   @Header('custom-header', 'none')
+  //   @HttpCode(204)
+  //   create(): string {
+  //     return 'This action adds a new cat';
+  //   }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    console.log(createCatDto);
+    return `This action adds a new cat ${createCatDto.name}`;
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Get('ab*cd')
+  wildcardHandler() {
+    return 'This route uses a wildcard';
+  }
+
+  @Get('mine')
+  @Redirect('http://localhost:3000/cats/ab*cd', 301)
+  handleMine() {
+    return {
+      url: 'http://localhost:3000/cats',
+      statusCode: 200,
+    };
+  }
+
+  @Get('/mine:id')
+  mineId(@Param('id') id: number): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+}
